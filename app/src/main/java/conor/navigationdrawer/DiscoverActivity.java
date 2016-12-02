91851c6323d537eee4ee82e7904616f30835f534
mecolmg <mecolmg@vt.edu>
2016-12-02 02:58:41 -0500

    Added API hookups and basic discovery page

--- app/src/main/java/conor/navigationdrawer/DiscoverActivity.java
+++ app/src/main/java/conor/navigationdrawer/DiscoverActivity.java
@@ -1,6 +1,7 @@
 package conor.navigationdrawer;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,18 +12,30 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class DiscoverActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private GridView recipeGrid;
+    private Button loadMore;
+    private RecipeAdapter recipeGridAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,23 +57,34 @@ public class DiscoverActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_drawer);
         navigationView.setNavigationItemSelectedListener(this);
 
-
-
-//        HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=10")
-//                .header("X-Mashape-Key", "oDmKpHGTrBmsh7yvrodVwUOEVEh6p12q0RTjsnE6X7fIiw6O9W")
-//                .header("Accept", "application/json")
-//                .asJson();
-
+        new RestGetTask().execute("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=10");
         recipeGrid = (GridView) findViewById(R.id.recipe_grid);
-        recipeGrid.setAdapter(new RecipeAdapter(this, new ArrayList<JSONObject>()));
+        recipeGridAdapter = new RecipeAdapter(this);
+        recipeGrid.setAdapter(recipeGridAdapter);
         recipeGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(DiscoverActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
+                JSONObject recipe = (JSONObject) recipeGridAdapter.getItem(position);
+                try {
+                    Toast.makeText(DiscoverActivity.this, "" + recipe.getInt("id"),
+                            Toast.LENGTH_SHORT).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+        recipeGrid.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if(firstVisibleItem + visibleItemCount >= totalItemCount){
+                    new RestGetTask().execute("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=10");
+                }
+            }
+        });
     }
 
     @Override
@@ -136,5 +160,63 @@ public class DiscoverActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    class RestGetTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            String response = null;
+            try {
+                URL recipeUrl = new URL(params[0]);
+                connection = (HttpURLConnection) recipeUrl.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("X-Mashape-Key", "oDmKpHGTrBmsh7yvrodVwUOEVEh6p12q0RTjsnE6X7fIiw6O9W");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.connect();
+
+                int status = connection.getResponseCode();
+
+                switch (status) {
+                    case 200:
+                    case 201:
+                        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line+"\n");
+                        }
+                        br.close();
+                        response = sb.toString();
+                }
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if(connection != null){
+                    connection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONObject response = new JSONObject(s);
+                JSONArray newRecipes = response.getJSONArray("recipes");
+                ArrayList<JSONObject> recipeList = new ArrayList<>();
+                for(int i=0; i < newRecipes.length(); i++){
+                    recipeList.add(newRecipes.getJSONObject(i));
+                }
+                recipeGridAdapter.addItems(recipeList);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            super.onPostExecute(s);
+        }
     }
 }
--- app/src/main/java/conor/navigationdrawer/RecipeAdapter.java
+++ app/src/main/java/conor/navigationdrawer/RecipeAdapter.java
@@ -1,14 +1,25 @@
 package conor.navigationdrawer;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Colm Gallagher on 12/1/2016.
@@ -19,21 +30,39 @@ public class RecipeAdapter extends BaseAdapter {
     private Context mContext;
     private LayoutInflater mInflater;
     private ArrayList<JSONObject> recipes;
+    private HashMap<Integer, Bitmap> cachedImages;
+    private HashMap<Integer, DownloadImageTask> downloadTasks;
 
-    public RecipeAdapter(Context context, ArrayList<JSONObject> items) {
+    public RecipeAdapter(Context context) {
         mContext = context;
-        recipes = items;
+        recipes = new ArrayList<>();
+        cachedImages = new HashMap<>();
+        downloadTasks = new HashMap<>();
         mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    public void addItems(ArrayList<JSONObject> newItems){
+        recipes.addAll(newItems);
+        for(JSONObject recipe : newItems){
+            try {
+                int id = recipe.getInt("id");
+                downloadTasks.put(id,new DownloadImageTask(null, id));
+                downloadTasks.get(id).execute(recipe.getString("image"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        this.notifyDataSetChanged();
     }
 
     @Override
     public int getCount() {
-        return 10;
+        return recipes.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return recipes.get(position);
     }
 
     @Override
@@ -43,8 +72,76 @@ public class RecipeAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        JSONObject json = (JSONObject) getItem(position);
+
         View rowView = mInflater.inflate(R.layout.grid_recipe_item, parent, false);
+        ImageView recipeImg = (ImageView) rowView.findViewById(R.id.recipe_image);
+        TextView recipeName = (TextView) rowView.findViewById(R.id.recipe_text);
+
+        try {
+            int id = json.getInt("id");
+            if(cachedImages.containsKey(id)){
+                recipeImg.setImageBitmap(cachedImages.get(id));
+            } else {
+                downloadTasks.get(id).setImageView(recipeImg);
+            }
+            recipeName.setText(json.getString("title"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         rowView.getWidth();
         return rowView;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+        int id;
+        Bitmap result;
+
+        public DownloadImageTask(ImageView bmImage, int id) {
+            this.bmImage = bmImage;
+            this.id = id;
+            this.result = null;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        public void setImageView(ImageView view){
+            bmImage = view;
+            if(result != null){
+                onPostExecute(result);
+            }
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            this.result = result;
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            result.compress(Bitmap.CompressFormat.JPEG, 50, out);
+            Bitmap thumbnail = BitmapFactory.decodeStream(new ByteArrayInputStream(out.toByteArray()));
+            if(!cachedImages.containsKey(id)) {
+                cachedImages.put(id, thumbnail);
+            }
+            if(bmImage != null){
+                bmImage.setImageBitmap(thumbnail);
+                try {
+                    this.finalize();
+                } catch (Throwable throwable) {
+                    Log.d("DOWNLOAD", "FINALIZE FAILED");
+                    throwable.printStackTrace();
+                }
+            }
+        }
     }
 }
